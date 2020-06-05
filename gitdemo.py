b2b27d74f751a50d785da051d7a3b90d5fcf9211
焦国宁 <45160904+jiaoguoning@users.noreply.github.com>
2020-06-05 15:45:38 +0800

    提出了一个需求

--- gitdemo.py
+++ gitdemo.py
@@ -5,8 +5,10 @@ Created on Fri Jun  5 08:26:36 2020
 
 @author: jiaoguoning
 """
+import numpy as np
+import matplotlib.pyplob as plt
 
-
+# 需求：画一个线形图
 print('hello')
 print('我在github做了更改')
 print('我在本地仓库做了更改')
