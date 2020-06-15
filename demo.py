20b56d37835dc042cd06bf127cc46be8475b22a5
jiaoguoning <jgn1361603343@163.com>
2020-06-15 08:37:33 +0800

    主题分支实验文件

---
+++ demo.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+# 测试使用Mac终端命令对文件进行一系列操作
+
+print('hello word')
+
+def functions(a, *b):
+    '''加法运算'''
+    sum = 0 
+    sum += a 
+    if b != null:
+	sum += sum(b)
+    return sum 
+
+a = 130 
+b = [1, 2, 3]
+
+functions(a, b.append(9))
